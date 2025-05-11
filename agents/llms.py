4ba5fa8cc4cff3b8692fd55a2f1035cbaac7ab9e
Alexis Nathaniel Silva <63516549+ilCat@users.noreply.github.com>
2025-05-11 20:01:00 +0200

    Update llms.py

--- agents/llms.py
+++ agents/llms.py
@@ -1,28 +1,10 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-# os.environ["GOOGLE_API_KEY"] = "AIzaSyDzXfFOES7LPfJR1JOt_exPXg20uYLK_Cw"
-
-# googllm = ChatGoogleGenerativeAI(
-#     model="gemini-2.0-flash",
-#     temperature=0,
-#     max_tokens=None,
-#     timeout=None,
-#     max_retries=2,
-# )
 
 
 class GoogleLLM(ChatGoogleGenerativeAI):
     pass
-    # def __init__(self, model="gemini-2.0-flash", temperature=0):
-
-    #     super().__init__(
-    #         self,
-    #         model=model,
-    #         temperature=temperature,
-    #         max_retries=2,
-    #     )
-
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
